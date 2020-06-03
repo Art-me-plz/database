c9dea68a61c4c3e7900a23be1fc1c1c00dbb112b
ab228 <56293580+ab228@users.noreply.github.com>
2020-06-03 10:10:48 +0500

    Update main.c

--- main.c
+++ main.c
@@ -7,38 +7,37 @@
 
 struct book
 {
-  long int id;
+  long long id;
   char autors[20];
   char name[30];
   int allbooks;
   int availablebooks;
 };
 
-struct book database[MAXSIZEOFBASE]; /*здесь будет вся информация базы данных*/
+struct book database[MAXSIZEOFBASE]; //здесь будет вся информация базы данных
+int Number = 1; // Количество занятых элементов в массиве
+FILE* books; 
 
-int Number; /* Количество занятых элементов в массиве */
-
-int menu();
+int menu(void);
 void add (void);
-void print (void);
+void Del(void);
+void out (void);
 
 int main(int argc, char const *argv[])
 {
-  Number = 0;
-  int from_menu = menu();
-  int num = 0;
-  struct book* sp = calloc(1, sizeof(struct book));
-  while(from_menu)
+  int from_menu = 0;
+  
+  while(from_menu != 5)
   {
+    out();
+    from_menu = menu();
     switch(from_menu)
     {
       case 1:
         add();
-        from_menu = menu();
       break;
-      case 3:
-        print();
-        from_menu = menu();
+      case 2:
+        Del();
       break;
     }
   }
@@ -47,58 +46,133 @@ int main(int argc, char const *argv[])
 
 void add (void)
 {
-      if (Number == MAXSIZEOFBASE - 1)
-      {
-        fprintf(stderr, "u can't add one more book\n");
-        return;
-      }
-      srand(time(NULL));
-      database[Number].id = rand();
-      printf("autors > ");
-      scanf("%s", database[Number].autors);
-      printf("name of a book > ");
-      scanf("%s", database[Number].name);
-      printf("The number of all this books > ");
-      scanf("%d", &database[Number].allbooks);
-      printf("The number of all available books (not more than all) > ");
-      scanf("%d", &database[Number].availablebooks);
-      Number ++;
+  if (Number == MAXSIZEOFBASE - 1)
+  {
+    fprintf(stderr, "u can't add one more book\n");
+    return;
   }
+  srand(time(NULL)); // для индивидуального номера используем случайное число
+  database[Number].id = rand();
+  printf("Autors > ");
+  scanf("%s", database[Number].autors);
+  printf("Name of a book > ");
+  scanf("%s", database[Number].name);
+  printf("The number of all this books > ");
+  scanf("%d", &database[Number].allbooks);
+  printf("The number of all available books (not more than all) > ");
+  scanf("%d", &database[Number].availablebooks);
+
+  books = fopen("books.csv", "w"); // запишем все в файл для дальнейшей работы
+  for (int x = 1; x <= Number; x++) {
+    fprintf(books, "%lld;%s;%s;%d;%d\n", database[x].id, database[x].autors,
+      database[x].name, database[x].allbooks, database[x].availablebooks);
+  }
+  fclose(books);
+  Number ++;
+  printf("successfully added\n");
+}
 
 int menu()
 {
-  int menu;
-  while(1)
-  {
-    puts("Menu");
-    puts("1 - add");
-    puts("2 - delite");
-    puts("3 - list");
-    puts("0 - to quite");
-    char c = getchar();
-    switch(c)
-    {
-      case '1': menu = 1; break;
-      case '2': menu = 2; break;
-      case '3': menu = 3; break;
-      case '0': menu = 0; break;
-      default: puts("Unknow simbol"); while (c != '\n') c = getchar(); continue;
+  int i = 0;
+  printf("select action:\n");
+  printf("1) Add new book\n");
+  printf("2) Delete the book\n");
+  printf("3) Edit the information\n");
+  printf("4) Change the number of books in the library\n");
+  printf("5) Save and exit\n");
+  printf("\n");
+  printf(">>>");
+  scanf("%d", &i);
+  return i;
+}
+
+void Del()
+{
+  int poisk = 0; // переменная для поиска номера в массиве всей базы
+  long long iddel = 0; // id удаляемого компонента
+  int nd = 0; // (number_of_delete) номер в массиве всей базы 
+
+  out();
+
+  books = fopen("books.csv", "w");
+  while (poisk == 0) { // ищем номер в массиве всей базы
+    printf("Enter the ID of the book you want to delete: ");
+    scanf("%lld", &iddel);
+    for (int k = 0; k < Number; k++) {
+      if (iddel == database[k].id) {
+        poisk = 1;
+        nd = k;
+      }
     }
-    while (c != '\n') c = getchar();
-    break;
+    if (poisk == 0) {
+      printf("book with this ID was not found\n");
+    }
   }
+  poisk = 0;
+  for (int x = 1; x < nd; x++) { // записываем все элементы до удаляемого в файл
+    fprintf(books, "%lld;%s;%s;%d;%d\n", database[x].id, database[x].autors, 
+      database[x].name, database[x].allbooks, database[x].availablebooks);
+  }
+  for (nd; nd < Number; nd++) { // сдвигаем элементы массива после удаляемого на 1 ячейку назад и записываем в файл
+    database[nd].id = database[nd + 1].id;
+    strncpy(database[nd].autors, database[nd + 1].autors, 100);
+    strncpy(database[nd].name, database[nd + 1].name, 100);
+    database[nd].allbooks = database[nd + 1].allbooks;
+    database[nd].availablebooks = database[nd + 1].availablebooks;
+    fprintf(books, "%lld;%s;%s;%d;%d\n", database[nd].id, database[nd].autors,
+      database[nd].name, database[nd].allbooks, database[nd].availablebooks);
+  }
+  fclose(books);
+  Number--;
+  database[Number].id = NULL;
+  printf("successfully deleted\n");
+}
+
+void out (void) 
+{
+  books = fopen("books.csv", "r");
+  if (books == NULL) {
+    printf("Failed to open the file");
+  }
+  char a[] = "ID";
+  char b[] = "Autors";
+  char c[] = "Name";
+  char d[] = "All books";
+  char e[] = "Available";
+  printf("%19s%30s%30s%15s%17s\n", a,b,c,d,e);
   
-  return menu;
-}
+  char mas[100];
+  fscanf(books, "%lld;", &database[Number].id);
+  int j = 0;
+  while (database[Number].id > 0) {            //цикл вывода библиотеки
+    printf("%19lld", database[Number].id);
+    while ((mas[j] = fgetc(books)) != '\n') {
+      if (mas[j] == ';') {
+        mas[j] = '\0';
+        printf("%30s", mas);
+        strncpy(database[Number].autors, mas, 100);
+        j = 0;
+        break;
+      }
+      else j++;
+    }
+    while ((mas[j] = fgetc(books)) != '\n') {
+      if (mas[j] == ';') {
+        mas[j] = '\0';
+        printf("%30s ", mas);
+        strncpy(database[Number].name, mas, 100);
+        j = 0;
+        break;
+      }
+      else j++;
+    }
 
-void print( void ) {
-    int i;
-
-  printf(" N  id         Autors              Name of book            All   Available\n");
-  for (i = 0; i < Number; i ++) {
-    printf("%2d. %11ld %-20s %-30s %-5d %-5d\n", i + 1, database[i].id, database[i].autors, 
-      database[i].name, database[Number].allbooks,
-      database[Number].availablebooks); 
+    fscanf(books, "%d;%d", &database[Number].allbooks, &database[Number].availablebooks);
+    printf("%13d ", database[Number].allbooks);
+    printf("%15d\n", database[Number].allbooks);
+    Number++;
+    fscanf(books, "%lld;", &database[Number].id);
   }
+  fclose(books);
 }
-
